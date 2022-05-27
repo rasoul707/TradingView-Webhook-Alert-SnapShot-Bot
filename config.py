1cd011e68e86196a27b1429ae2c71ccd36557fa5
Rasoul Ahmadifar <r.ahmadifar.1377@gmail.com>
2022-05-28 00:17:38 +0430

    Update config.py

--- config.py
+++ config.py
@@ -4,24 +4,26 @@
 # File Name             : config.py                                     #
 # ------------------------------------------------------------------- #
 
-BOT_TOKEN = "5386492523:AAFxvoCs0mFyMAx8yhklnVHHJiCM_ks4qM4"
+BOT_TOKEN = "BOT_TOKEN"
 
 admin = 115025624
 
 keys = [
-    "KEY_MyChannel",
-    "xRdom45SrcToMxQ53946Top"
+    "key1",
+    "key2",
+    "key3"
 ]
 
 channels = [
-    -1001368926240,
-    -1001724862991
+    -1,
+    -2,
+    -3
 ]
 
 
 # trading-view auth
-username = "qkhpmdbekdeal"
-password = "5HDzYmPpCWQs"
+username = "username"
+password = "password"
 
 
 # default chart settings
