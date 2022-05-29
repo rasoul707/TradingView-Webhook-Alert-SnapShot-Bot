13cbabc53966544964529d8f47624d89de34f403
Rasoul Ahmadifar <r.ahmadifar.1377@gmail.com>
2022-05-29 12:33:15 +0430

    node-fetch import

--- capture.js
+++ capture.js
@@ -10,7 +10,7 @@ const express = require('express');
 const puppeteer = require('puppeteer');
 const UserAgent = require('user-agents');
 const moment = require('moment');
-const fetch = require('node-fetch');
+import fetch from 'node-fetch';
 const app = express();
 
 let browser, useragent;
