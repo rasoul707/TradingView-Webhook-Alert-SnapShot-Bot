9a774c3cf3a95d2a7e906168151458d2c929ee0c
Rasoul Ahmadifar <r.ahmadifar.1377@gmail.com>
2022-06-09 11:17:24 +0430

    Update capture.js

--- capture.js
+++ capture.js
@@ -326,6 +326,7 @@ app.get('/capture', async function (req, res) {
     }).catch((err) => {
         console.log('Failed', err)
         res.json({ ok: false, error: err.toString(), images })
+        throw err;
     })
 
 
