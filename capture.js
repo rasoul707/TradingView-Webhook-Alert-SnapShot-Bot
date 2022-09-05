63f5447237cc35979e97d450f89c9d8bae1ef577
Rasoul Ahmadifar <r.ahmadifar.1377@gmail.com>
2022-09-05 17:22:16 +0430

    change port to 443

--- capture.js
+++ capture.js
@@ -86,8 +86,8 @@ const runPythonBot = () => {
 
 
 
-app.listen(7007, () => {
-    console.log('Server is running on port 7007');
+app.listen(443, () => {
+    console.log('Server is running on port 443');
     // runPythonBot();
 });
 
