c0a93514f15ab0e9a29e28958871ac3ef7de6cea
Rasoul Ahmadifar <r.ahmadifar.1377@gmail.com>
2022-09-05 10:39:38 +0430

    watermark

--- snapshot.py
+++ snapshot.py
@@ -45,8 +45,8 @@ def cropImage(imgPath):
 
 def watermark(imgPath, topWatermark):
     print("gggg")
-    print(imgPath)
-    print(topWatermark)
+    # print(imgPath)
+    # print(topWatermark)
     # requestUrl = f'http://localhost:7007/snapshots/watermark?filePath={imgPath}&topWatermark={topWatermark}'
     # print(requestUrl)
     # requests.get(requestUrl)
